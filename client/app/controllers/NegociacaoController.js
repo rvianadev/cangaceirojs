e1ee6d3e415e73a432f940dcf17ccac9284c5a75
Rodrigo Viana de Oliveira <roviol@gmail.com>
2021-01-14 10:37:01 -0300

    Cap 7 - Item 7.2

--- client/app/controllers/NegociacaoController.js
+++ client/app/controllers/NegociacaoController.js
@@ -9,6 +9,9 @@ class NegociacaoController {
     this._negociacoes = new Negociacoes();
     this._negociacoesView = new NegociacoesView('#negociacoes');
     this._negociacoesView.update(this._negociacoes);
+    this._mensagem = new Mensagem();
+    this._mensagemView = new MensagemView('#mensagemView');
+    this._mensagemView.update(this._mensagem);
   }
 
 
@@ -16,7 +19,9 @@ class NegociacaoController {
     
     event.preventDefault();
     this._negociacoes.adiciona(this._criaNegociacao());
+    this._mensagem.texto = 'Negociação adicionada com sucesso';
     this._negociacoesView.update(this._negociacoes);
+    this._mensagemView.update(this._mensagem);
     this._limpaFormulario();
   }
 
---
+++ client/app/ui/views/MensagemView.js
@@ -0,0 +1,17 @@
+class MensagemView {
+
+  constructor(seletor) {
+
+    this._elemento = document.querySelector(seletor);
+  }
+
+  template(model) {
+
+    return `<p class="alert alert-info">${model.texto}</p>`;
+  }
+
+  update(model) {
+
+    this._elemento.innerHTML = this.template(model);
+  }
+}
