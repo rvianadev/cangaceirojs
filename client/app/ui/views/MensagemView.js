2d80ed86640ebf7ae76b3bec5813c8764bcc4598
Rodrigo Viana de Oliveira <roviol@gmail.com>
2021-01-15 10:36:13 -0300

    Cap 7 - Item 7.3

--- client/app/ui/views/MensagemView.js
+++ client/app/ui/views/MensagemView.js
@@ -1,17 +1,9 @@
-class MensagemView {
-
-  constructor(seletor) {
-
-    this._elemento = document.querySelector(seletor);
-  }
+class MensagemView extends View {
 
   template(model) {
 
-    return `<p class="alert alert-info">${model.texto}</p>`;
-  }
-
-  update(model) {
-
-    this._elemento.innerHTML = this.template(model);
+    return model.texto 
+      ? `<p class="alert alert-info">${model.texto}</p>`
+      : `<p></p>`;
   }
 }
